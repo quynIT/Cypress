a49f57ad276406d4dadac154e98f86fee28a3c20
quynIT <trungquyen2902@gmail.com>
2024-11-08 20:48:10 +0700

    Test-register

--- cypress/e2e/Testing/RegisterTesting.cy.js
+++ cypress/e2e/Testing/RegisterTesting.cy.js
@@ -1,5 +1,107 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://account.nganluong.vn/nganluong/register/personal");
+    // Click vao button dang ky
+    cy.get(".base-button").click();
+    // Username kí tự db
+    cy.get("#fullname").type("@$#^&*");
+    // Username number
+    cy.get("#fullname").type("1243525246");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+    // Username 1 kí tự
+    cy.get("#fullname").clear();
+    cy.get("#fullname").type("abcdef");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+    // Username 2 kí tự
+    cy.get("#fullname").clear();
+    cy.get("#fullname").type("Nguyễn Văn A");
+    cy.get(".base-button").click({ force: true });
+
+    cy.get("#password").invoke("attr", "type", "text");
+    cy.get("#password").type("     ");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    // Password kí tự db
+    cy.get("#password").clear();
+    cy.get("#password").type("@@$$^$^");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    // Password chu cai hoa
+    cy.get("#password").clear();
+    cy.get("#password").type("ABCabcd");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    // Password 6 ki tu
+    cy.get("#password").clear();
+    cy.get("#password").type("abcdef");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    // Password 7 ki tu
+    cy.get("#password").clear();
+    cy.get("#password").type("jCy8T86#5GXuh8H");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    // Password not match
+    cy.get("#confirm_password").invoke("attr", "type", "text");
+    cy.get("#confirm_password").type("abcdefgh");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    // Password exact match
+    cy.get("#confirm_password").clear();
+    cy.get("#confirm_password").type("jCy8T86#5GXuh8H");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    // Email @ or .
+    cy.get("#email").type("abcdefgh");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    //Email number
+    cy.get("#email").clear();
+    cy.get("#email").type("78247698232");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    //Email
+    cy.get("#email").clear();
+    cy.get("#email").type("vjppro123@gmail.com");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    //Phone number ki tu db
+    cy.get("#phone_number").type("@@@@@12453abc");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    //Phone number 9 so
+    cy.get("#phone_number").clear();
+    cy.get("#phone_number").type("123456789");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    //Phone number 11 so
+    cy.get("#phone_number").clear();
+    cy.get("#phone_number").type("12345678910");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    //Phone number 10 so có số 0 đầu
+    cy.get("#phone_number").clear();
+    cy.get("#phone_number").type("0329903574");
+    cy.get(".base-button").click({ force: true });
+    cy.wait(2000);
+
+    //Checkbox
+    cy.get('[style="width: 304px; height: 78px;"] > div > iframe').click();
+    cy.get(".base-button").click({ force: true });
+  });
+});
