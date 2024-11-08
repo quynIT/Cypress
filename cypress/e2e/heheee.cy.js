09f7ad816817ab1ca5497e927dbb47dfa957c2f3
DRobbie39 <phangiadat123@gmail.com>
2024-11-08 18:09:12 +0700

    Create two testing places

--- cypress/e2e/heheee.cy.js
+++
@@ -1,9 +0,0 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://formspree.io/login");
-    cy.get(".fvmeZf").click();
-    // Xác thực nếu có thông báo lỗi xuất hiện
-    cy.contains("Incorrect username or password.").should("be.visible");
-    cy.pause();
-  });
-});
